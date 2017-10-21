04edc73f54277eee7969b683016e6a52776304d5
vaerjngiar <vaerjngiar@gmail.com>
2017-10-21 18:01:56 +0300

    basic routes added

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const http = require('http');
 
-const public = require('./routes/public');
+
+
+const {public, home, search, notFound} = require('./routes');
+
 
 http.createServer((req, res) => {
     if (req.url.match(/\.(html)|(css)|(png)|(js)|(jpg)/)){
         public(req, res);
     } else if (req.url === '/'){
-
+        home(req, res);
     }else if (req.url.startsWith('/search')){
-
+        search(req, res);
     }else {
-
+        notFound(req, res);
     }
 
 }).listen(3000, ()=>{ console.log('Сервер работоает на http://localhost:3000')});
