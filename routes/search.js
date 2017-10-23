f088c5039fc043c6188aa8cb9ec6c2e1bc2e490d
vaerjngiar <vaerjngiar@gmail.com>
2017-10-23 11:36:52 +0300

    recieve data

--- routes/search.js
+++ routes/search.js
@@ -1,10 +1,23 @@
 const fs = require('fs');
 const path = require('path');
 
+const omdb = require('../lib/omdb');
+const url = require('url');
+
 
 function search (req, res){
+    const parsedUrl = url.parse(req.url, true);
+    const title = parsedUrl.query.title;
+
+    console.log(parsedUrl.query);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
+
+    omdb.get(title, (error, movie) =>{
+        if(error) throw error;
+
+       console.log(movie);
+    });
 
     const stream = fs.createReadStream(path.resolve('public', 'movie.html'));
 
